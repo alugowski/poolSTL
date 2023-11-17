aaacc758372d5583504a90a5fc2a979b35df4ecf
Adam Lugowski <alugowski@gmail.com>
2023-11-16 22:39:38 -0800

    Add par_if

--- include/poolstl/seq_fwd.hpp
+++ include/poolstl/seq_fwd.hpp
@@ -8,68 +8,84 @@
 
 #include "execution"
 
-namespace poolstl {
-    namespace execution {
-        struct sequential_policy {};
-        constexpr sequential_policy seq{};
+/*
+ * Forward poolstl::seq to the native sequential (no policy) method.
+ */
+
+#define POOLSTL_DEFINE_SEQ_FWD(FNAME)                                                                       \
+    template<class EP, typename...ARGS>                                                                     \
+    auto FNAME(EP&&, ARGS&&...args) ->                                                                      \
+                poolstl::internal::enable_if_seq<EP, decltype(std::FNAME(std::forward<ARGS>(args)...))> {   \
+        return std::FNAME(std::forward<ARGS>(args)...);                                                     \
     }
 
-    using execution::seq;
-
-    namespace internal {
-        // short name for short lines below
-        template <class ExecPolicy, class Tp>
-        using ifseq =
-            typename std::enable_if<
-                std::is_same<poolstl::execution::sequential_policy,
-                    typename std::remove_cv<typename std::remove_reference<ExecPolicy>::type>::type>::value,
-                Tp>::type;
-
+#define POOLSTL_DEFINE_SEQ_FWD_VOID(FNAME)                                       \
+    template<class EP, typename...ARGS>                                          \
+    poolstl::internal::enable_if_seq<EP, void> FNAME(EP&&, ARGS&&... args) {     \
+        std::FNAME(std::forward<ARGS>(args)...);                                 \
     }
-}
+
+/*
+ * Dynamically choose between parallel and sequential policy
+ */
+
+#define POOLSTL_DEFINE_PAR_IF_FWD_VOID(FNAME)                                              \
+    template<class EP, typename...ARGS>                                                    \
+    poolstl::internal::enable_if_par_if<EP, void> FNAME(EP&& policy, ARGS&&...args) {      \
+        if (policy.call_par) {                                                             \
+            std::FNAME(policy.get_par(), std::forward<ARGS>(args)...);                     \
+        } else {                                                                           \
+            std::FNAME(policy.get_seq(), std::forward<ARGS>(args)...);                     \
+        }                                                                                  \
+    }
+
+#define POOLSTL_DEFINE_PAR_IF_FWD(FNAME)                                                                       \
+    template<class EP, typename...ARGS>                                                                        \
+    auto FNAME(EP&& policy, ARGS&&...args) ->                                                                  \
+                poolstl::internal::enable_if_par_if<EP, decltype(std::FNAME(std::forward<ARGS>(args)...))> {   \
+        if (policy.call_par) {                                                                                 \
+            return std::FNAME(policy.get_par(), std::forward<ARGS>(args)...);                                  \
+        } else {                                                                                               \
+            return std::FNAME(policy.get_seq(), std::forward<ARGS>(args)...);                                  \
+        }                                                                                                      \
+    }
+
+/*
+ * Define both the sequential forward and dynamic chooser.
+ */
+#define POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(FNAME)        \
+                    POOLSTL_DEFINE_SEQ_FWD(FNAME)            \
+                    POOLSTL_DEFINE_PAR_IF_FWD(FNAME)
+
+#define POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF_VOID(FNAME)   \
+                    POOLSTL_DEFINE_SEQ_FWD_VOID(FNAME)       \
+                    POOLSTL_DEFINE_PAR_IF_FWD_VOID(FNAME)
 
 namespace std {
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(all_of)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(any_of)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(none_of)
 
-    /*
-     * Forward poolstl::seq to the native sequential (no policy) method.
-     */
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(copy)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(copy_n)
 
-#define POOLSTL_DEFINE_SEQ_FWD(FNAME)                                                                                  \
-    template<class EP, typename...ARGS>                                                                                \
-    auto FNAME(EP&&, ARGS&&...args) -> poolstl::internal::ifseq<EP,decltype(std::FNAME(std::forward<ARGS>(args)...))> {\
-        return std::FNAME(std::forward<ARGS>(args)...);                                                                \
-    }
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF_VOID(fill)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(fill_n)
 
-#define POOLSTL_DEFINE_SEQ_FWD_VOID(FNAME)                              \
-    template<class EP, typename...ARGS>                                 \
-    poolstl::internal::ifseq<EP, void> FNAME(EP&&, ARGS&&...args) {     \
-        std::FNAME(std::forward<ARGS>(args)...);                        \
-    }
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(find)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(find_if)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(find_if_not)
 
-    POOLSTL_DEFINE_SEQ_FWD(all_of)
-    POOLSTL_DEFINE_SEQ_FWD(any_of)
-    POOLSTL_DEFINE_SEQ_FWD(none_of)
-
-    POOLSTL_DEFINE_SEQ_FWD(copy)
-    POOLSTL_DEFINE_SEQ_FWD(copy_n)
-
-    POOLSTL_DEFINE_SEQ_FWD_VOID(fill)
-    POOLSTL_DEFINE_SEQ_FWD(fill_n)
-
-    POOLSTL_DEFINE_SEQ_FWD(find)
-    POOLSTL_DEFINE_SEQ_FWD(find_if)
-    POOLSTL_DEFINE_SEQ_FWD(find_if_not)
-
-    POOLSTL_DEFINE_SEQ_FWD_VOID(for_each)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF_VOID(for_each)
 #if POOLSTL_HAVE_CXX17_LIB
-    POOLSTL_DEFINE_SEQ_FWD(for_each_n)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(for_each_n)
 #endif
 
-    POOLSTL_DEFINE_SEQ_FWD(transform)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(transform)
 
 #if POOLSTL_HAVE_CXX17_LIB
-    POOLSTL_DEFINE_SEQ_FWD(reduce)
-    POOLSTL_DEFINE_SEQ_FWD(transform_reduce)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(reduce)
+    POOLSTL_DEFINE_BOTH_SEQ_FWD_AND_PAR_IF(transform_reduce)
 #endif
 }
 

6a79e933ff03f62aee1e43ff0e65faa7ceb880e9
Adam Lugowski <alugowski@gmail.com>
2023-11-27 19:33:29 -0800

    Add Windows MinGW tests

--- tests/supplement_test.cpp
+++ tests/supplement_test.cpp
@@ -10,7 +10,9 @@
 // The <execution> header defines compiler-provided execution policies, but is not always present.
 #if __cplusplus >= 201603L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201603L)
 #if __has_include(<execution>)
-#ifndef POOLSTL_MISSING_NEEDED_TBB
+#ifndef POOLSTL_STD_SUPPLEMENT_NO_INCLUDE
+// On GCC and Clang simply including <execution> requires linking with TBB.
+// MinGW does not require TBB, but performance may be sequential. To keep parallel performance fallback to poolSTL.
 #include <execution>
 #endif
 #endif
